2079d466c1f6c5c00d96f97879fc0f71ff942a0f
Elon Niu <elonniu@amazon.com>
2022-11-09 23:30:48 +0800

    fixed

---
+++ src/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -d "/srv/opcache" -a ! -d "/tmp/opcache" ]
+then
+    mkdir /tmp/opcache
+    umask 0000 && time cp -r /srv/opcache/* /tmp/opcache
+fi
+
+if [ ! -d '/mnt/share/uploads' ]
+then
+   mkdir -p /mnt/share/uploads
+fi 
+
+/usr/sbin/php-fpm
+
+exec nginx -g "daemon off;";
